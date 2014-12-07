ece09cb0fee5241e70d6485c6a025fa54657d53f
David Noyes <david.j.noyes@gmail.com>
2014-12-07 14:33:26 +0000

    cert and key nice names. Make host key based on existing CA

--- EasyCert.go
+++ EasyCert.go
@@ -7,10 +7,12 @@ import (
 	"os"
 	"os/exec"
 	"runtime/debug"
+	"strings"
 )
 
 var (
 	flagCertificateAuthorityName = flag.String("cn", "", "")
+	flagCertificateAuthorityFile = flag.String("cf", "", "")
 	flagHostName                 = flag.String("h", "", "")
 )
 
@@ -18,6 +20,7 @@ var usage = `Usage: EasyCert [options...]
 
 Options:
   -cn Certificate Authority Name (can be any name, but should reflect your company name.)
+  -cf Certificate Authority File (use existing CA file, can not be used with -cn.)
   -h  Hostname of TLS server to install the private cert/key
 `
 
@@ -29,49 +32,67 @@ func main() {
 	flag.Parse()
 
 	certName := *flagCertificateAuthorityName
+	certFile := *flagCertificateAuthorityFile
 	hostName := *flagHostName
 
-	if certName == "" || hostName == "" {
-		usageAndExit("You must supply both a -cn (certificate name) and -h (host name) parameter")
+	if certName != "" && certFile != "" {
+		usageAndExit("There is no need to supply -cn (certificate name) when a -cf (certificate file) is already available")
 	}
 
-	createPrivateCA(certName)
-	createServerCertKey(hostName)
+	if certFile == "" {
+		if certName == "" || hostName == "" {
+			usageAndExit("You must supply both a -cn (certificate name) and -h (host name) parameter")
+		}
+	} else if certName == "" {
+		if certFile == "" || hostName == "" {
+			usageAndExit("You must supply both a -cf (certificate file) and -h (host name) parameter")
+		}
+		if _, err := os.Stat(certFile); os.IsNotExist(err) {
+			usageAndExit("The -cf (certificate file) can not be found")
+		}
+	}
 
-	log.Println("*** Operation Completed Succesfully ***")
-	log.Println("Private root certificate created: ", "myCA.cer")
-	log.Println("Web server certificate created: ", "mycert1.cer")
-	log.Println("Web server key created: ", "mycer1.key")
+	if certFile == "" {
+		certFile := createPrivateCA(certName)
+		log.Println("Private root certificate created: ", certFile+".cer")
+	}
+
+	hostFile := createServerCertKey(hostName, certFile)
+	log.Println("Web server certificate created: ", hostFile+".cer")
+	log.Println("Web server key created: ", hostFile+".key")
 }
 
-func createPrivateCA(certificateAuthorityName string) {
-	_, err := callCommand("openssl", "genrsa", "-out", "myCA.key", "2048")
+func createPrivateCA(certificateAuthorityName string) string {
+	certificateAuthorityFile := strings.Replace(certificateAuthorityName, ".", "_", -1) + "_CA"
+	_, err := callCommand("openssl", "genrsa", "-out", certificateAuthorityFile+".key", "2048")
 	if err != nil {
 		log.Fatal("Could not create private Certificate Authority key")
 	}
 
-	_, err = callCommand("openssl", "req", "-x509", "-new", "-key", "myCA.key", "-out", "myCA.cer", "-days", "730", "-subj", "/CN=\""+certificateAuthorityName+"\"")
+	_, err = callCommand("openssl", "req", "-x509", "-new", "-key", certificateAuthorityFile+".key", "-out", certificateAuthorityFile+".cer", "-days", "730", "-subj", "/CN=\""+certificateAuthorityName+"\"")
 	if err != nil {
 		log.Fatal("Could not create private Certificate Authority certificate")
 	}
+	return certificateAuthorityFile
 }
 
-func createServerCertKey(host string) {
-	_, err := callCommand("openssl", "genrsa", "-out", "mycert1.key", "2048")
+func createServerCertKey(host, certFile string) string {
+	hostFile := strings.Replace(host, ".", "_", -1)
+	_, err := callCommand("openssl", "genrsa", "-out", hostFile+".key", "2048")
 	if err != nil {
 		log.Fatal("Could not create private server key")
 	}
 
-	_, err = callCommand("openssl", "req", "-new", "-out", "mycert1.req", "-key", "mycert1.key", "-subj", "/CN="+host)
+	_, err = callCommand("openssl", "req", "-new", "-out", hostFile+".req", "-key", hostFile+".key", "-subj", "/CN="+host)
 	if err != nil {
 		log.Fatal("Could not create private server certificate signing request")
 	}
 
-	_, err = callCommand("openssl", "x509", "-req", "-in", "mycert1.req", "-out", "mycert1.cer", "-CAkey", "myCA.key", "-CA", "myCA.cer", "-days", "365", "-CAcreateserial", "-CAserial", "serial")
+	_, err = callCommand("openssl", "x509", "-req", "-in", hostFile+".req", "-out", hostFile+".cer", "-CAkey", certFile+".key", "-CA", certFile+".cer", "-days", "365", "-CAcreateserial", "-CAserial", "serial")
 	if err != nil {
 		log.Fatal("Could not create private server certificate")
 	}
-
+	return hostFile
 }
 
 func callCommand(command string, arg ...string) (string, error) {
