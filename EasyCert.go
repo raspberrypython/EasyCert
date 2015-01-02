3ffd2cc9cad3253291582f4570dab39057e32ad2
David Noyes <david.noyes@plus.net>
2015-01-02 11:50:43 +0000

    Changed cer file to crt. Added help output

--- EasyCert.go
+++ EasyCert.go
@@ -14,7 +14,7 @@ import (
 var (
 	flagCertificateAuthorityName    = flag.String("cn", "", "")
 	flagCertificateAuthorityKeyFile = flag.String("cakey", "", "")
-	flagCertificateAuthorityCerFile = flag.String("cacer", "", "")
+	flagCertificateAuthorityCrtFile = flag.String("cacrt", "", "")
 	flagFQDN                        = flag.String("fqdn", "", "")
 )
 
@@ -23,7 +23,7 @@ var usage = `Usage: EasyCert [options...]
 Options:
   -cn      Certificate Authority Name (can be any name, but should reflect your company name.)
   -cakey   Certificate Authority Key File (use existing CA Key file, can not be used with -cn.)
-  -cacer   Certificate Authority Key File (use existing CA Cer file, can not be used with -cn.)
+  -cacrt   Certificate Authority Key File (use existing CA Cer file, can not be used with -cn.)
   -fqdn    Fully qualified domain name of TLS server to install the private cert/key
 `
 
@@ -36,37 +36,41 @@ func main() {
 
 	caName := *flagCertificateAuthorityName
 	caKeyFile := *flagCertificateAuthorityKeyFile
-	caCerFile := *flagCertificateAuthorityCerFile
+	caCrtFile := *flagCertificateAuthorityCrtFile
 	fqdn := *flagFQDN
 
-	if caName != "" && (caKeyFile != "" || caCerFile != "") {
-		usageAndExit("There is no need to supply -cn (certificate authority name) when a -cakey (certificate authority key file) or -cacer (certificate authority cer file) is already available")
+	if caKeyFile == "" && caCrtFile == "" && caName == "" && fqdn == "" {
+		usageAndExit("No parameters supplied. You must supply -fqdn with either -cn or -cakey & -cacrt")
 	}
 
-	if caKeyFile == "" && caCerFile == "" {
+	if caName != "" && (caKeyFile != "" || caCrtFile != "") {
+		usageAndExit("There is no need to supply -cn (certificate authority name) when a -cakey (certificate authority key file) or -cacrt (certificate authority crt file) is already available")
+	}
+
+	if caKeyFile == "" && caCrtFile == "" {
 		if caName == "" || fqdn == "" {
 			usageAndExit("You must supply both a -cn (certificate authority name) and -fqdn (fully qualified domain name) parameter")
 		}
 	} else if caName == "" {
-		if caKeyFile == "" || caCerFile == "" || fqdn == "" {
-			usageAndExit("You must supply a -cakey (certificate authority key file), -cacer (certificate authority cer file) and -fqdn (fully qualified domain name) parameter")
+		if caKeyFile == "" || caCrtFile == "" || fqdn == "" {
+			usageAndExit("You must supply a -cakey (certificate authority key file), -cacrt (certificate authority cer file) and -fqdn (fully qualified domain name) parameter")
 		}
 		if _, err := os.Stat(caKeyFile); os.IsNotExist(err) {
 			usageAndExit("The -cakey (certificate authority key file) can not be found")
 		}
-		if _, err := os.Stat(caCerFile); os.IsNotExist(err) {
-			usageAndExit("The -cacer (certificate authority cer file) can not be found")
+		if _, err := os.Stat(caCrtFile); os.IsNotExist(err) {
+			usageAndExit("The -cactr (certificate authority cer file) can not be found")
 		}
 	}
 
-	if caKeyFile == "" && caCerFile == "" {
-		caKeyFile, caCerFile = createPrivateCA(caName)
-		log.Println("Private root certificate created: ", caCerFile)
+	if caKeyFile == "" && caCrtFile == "" {
+		caKeyFile, caCrtFile = createPrivateCA(caName)
+		log.Println("Private root certificate created: ", caCrtFile)
 		log.Println("Private root key created: ", caKeyFile)
 	}
 
-	fqdnKeyFile, fqdnCerFile := createServerCertKey(fqdn, caKeyFile, caCerFile)
-	log.Println("Web server certificate created: ", fqdnCerFile)
+	fqdnKeyFile, fqdnCrtFile := createServerCertKey(fqdn, caKeyFile, caCrtFile)
+	log.Println("Web server certificate created: ", fqdnCrtFile)
 	log.Println("Web server key created: ", fqdnKeyFile)
 }
 
@@ -74,25 +78,25 @@ func createPrivateCA(certificateAuthority string) (string, string) {
 	caName := strings.Replace(certificateAuthority, ".", "_", -1) + "_CA"
 	os.Mkdir(caName, 0700)
 	caKeyFile := path.Join(caName, caName+".key")
-	caCerFile := path.Join(caName, caName+".cer")
+	caCrtFile := path.Join(caName, caName+".crt")
 
 	_, err := callCommand("openssl", "genrsa", "-out", caKeyFile, "2048")
 	if err != nil {
 		log.Fatal("Could not create private Certificate Authority key")
 	}
 
-	_, err = callCommand("openssl", "req", "-x509", "-new", "-key", caKeyFile, "-out", caCerFile, "-days", "730", "-subj", "/CN=\""+certificateAuthority+"\"")
+	_, err = callCommand("openssl", "req", "-x509", "-new", "-key", caKeyFile, "-out", caCrtFile, "-days", "730", "-subj", "/CN=\""+certificateAuthority+"\"")
 	if err != nil {
 		log.Fatal("Could not create private Certificate Authority certificate")
 	}
-	return caKeyFile, caCerFile
+	return caKeyFile, caCrtFile
 }
 
-func createServerCertKey(fqdn, caKeyFile string, caCerFile string) (string, string) {
+func createServerCertKey(fqdn, caKeyFile string, caCrtFile string) (string, string) {
 	fqdnName := strings.Replace(fqdn, ".", "_", -1)
 	os.Mkdir(fqdnName, 0700)
 	fqdnKeyFile := path.Join(fqdnName, fqdnName+".key")
-	fqdnCerFile := path.Join(fqdnName, fqdnName+".cer")
+	fqdnCrtFile := path.Join(fqdnName, fqdnName+".crt")
 	fqdnReqFile := path.Join(fqdnName, fqdnName+".req")
 
 	_, err := callCommand("openssl", "genrsa", "-out", fqdnKeyFile, "2048")
@@ -105,11 +109,11 @@ func createServerCertKey(fqdn, caKeyFile string, caCerFile string) (string, stri
 		log.Fatal("Could not create private server certificate signing request")
 	}
 
-	_, err = callCommand("openssl", "x509", "-req", "-in", fqdnReqFile, "-out", fqdnCerFile, "-CAkey", caKeyFile, "-CA", caCerFile, "-days", "365", "-CAcreateserial", "-CAserial", path.Join(fqdnName, "serial"))
+	_, err = callCommand("openssl", "x509", "-req", "-in", fqdnReqFile, "-out", fqdnCrtFile, "-CAkey", caKeyFile, "-CA", caCrtFile, "-days", "365", "-CAcreateserial", "-CAserial", path.Join(fqdnName, "serial"))
 	if err != nil {
 		log.Fatal("Could not create private server certificate")
 	}
-	return fqdnKeyFile, fqdnCerFile
+	return fqdnKeyFile, fqdnCrtFile
 }
 
 func callCommand(command string, arg ...string) (string, error) {
